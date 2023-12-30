5f6f8ccc55cdfcf6693fdf485be645b51aa439cd
ShrutiDeore76 <155220927+ShrutiDeore76@users.noreply.github.com>
2023-12-30 13:19:12 +0530

    Updated HelloWorld.java : written addition of two numbers code

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,14 @@ public class HelloWorld
     //this is a demo program
      public static void main(String[]args)
     {
-      System.out.println("Hello World");
-      System.out.println("Adding more text");
+        System.out.println("Hello World");
+        System.out.println("Adding more text");
+
+        //Addition of two numbers
+        int a=3;
+        int b=7;
+        int sum=0;
+        sum=a+b;
+        System.out.println("The addition of two numbers is : " + sum);
     }
 }
